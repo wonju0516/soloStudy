4e3b816d33a2902eb2ac4fadda44f71e4f5dd1f0
mok <dnjswn0516@gmail.com>
2026-01-23 17:13:39 +0900

    feat: 타이머와 버튼 ui 구성 완료

--- part2/voiceRecoderApp/app/src/main/java/com/part2/voiceapp/MainActivity.kt
+++ part2/voiceRecoderApp/app/src/main/java/com/part2/voiceapp/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.part2.voiceapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
